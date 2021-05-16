45baa06f3072cec521253e26c919b30f56d772c1
Berkay COBAN <bcobann35@gmail.com>
2021-05-16 15:10:04 +0300

    :zap: Add /users POST option for create a new user

--- Server.js
+++ Server.js
@@ -13,17 +13,36 @@ router.get("/", (req, res) => {
   res.json({ error: false, message: "Hello World!" });
 });
 
-router.route("/users").get((req, res) => {
-  let response = {};
-  mongoOp.find({}, (err, data) => {
-    if (err) {
-      response = { error: true, message: "Error fetching data!" };
-    } else {
-      response = { error: false, message: data };
+router
+  .route("/users")
+  .get((req, res) => {
+    let response = {};
+    mongoOp.find({}, (err, data) => {
+      if (err) {
+        response = { error: true, message: "Error fetching data!" };
+      } else {
+        response = { error: false, message: data };
+      }
+      res.json(response);
+    });
+  })
+  .post(async (req, res) => {
+    // console.log("req.body.username : " + req.body.username);
+    // console.log("req.body.last : " + req.body.name.last);
+
+    let response = {};
+
+    try {
+      let user = new mongoOp(req.body);
+      let result = await user.save();
+
+      response = { error: false, message: `User added! ${result}` };
+      res.status(200).json(response);
+    } catch (error) {
+      response = { error: true, message: error };
+      res.status(500).json(response);
     }
-    res.json(response);
   });
-});
 
 app.use(router);
 
