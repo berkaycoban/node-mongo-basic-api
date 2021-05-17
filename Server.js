2cf3b54398b43a0d6f8cea4e6a9387feb0c25e9c
Berkay COBAN <bcobann35@gmail.com>
2021-05-17 16:14:18 +0300

    :zap: Add Swagger

--- Server.js
+++ Server.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const app = express();
-
 const bodyParser = require("body-parser");
 const router = express.Router();
+const app = express();
 
+// body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// db
 const connectDB = require("./config/db");
 connectDB();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 router.get("/", (req, res) => {
   res.json({ error: false, message: "Hello World!" });
