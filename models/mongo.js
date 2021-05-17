860fce84bf33324b471c7d5c3ac6dd2c85d4367b
Berkay COBAN <bcobann35@gmail.com>
2021-05-17 14:37:04 +0300

    :fire: Delete models/mongo.js unused file.

--- models/mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-mongoose.connect(`mongodb://localhost:27017/people`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-module.exports = mongoose;
